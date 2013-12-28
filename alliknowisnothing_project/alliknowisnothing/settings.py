ff7a53c6736c692352915d7ccd1a443ce551eb34
tetesting <tetestingtesting@gmail.com>
2013-12-28 13:49:01 -0500

    Update settings for static files

--- alliknowisnothing_project/alliknowisnothing/settings.py
+++ alliknowisnothing_project/alliknowisnothing/settings.py
@@ -80,14 +80,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_deploy')
 
 from confidential import SECRET_KEY
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static_dev'),
 )
 
 INTERNAL_IPS = (
